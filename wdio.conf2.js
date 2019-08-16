ee7fdd398f440302d92774d668f051a031220a63
DenysBarkanov <denys@stakelogic.com>
2019-08-16 15:13:20 +0300

    second commit

--- wdio.conf2.js
+++ wdio.conf2.js
@@ -12,6 +12,7 @@ exports.config = {
     specs: [
         //'./test/**/*.js'
         './test/**/global-header.js'
+        //'./test/**/client.js'
         //'./test/**/AddRemoveElements.js'
         //'./test/**/brokenImages.js'
         //'./test/**/findSelectorsChallenge.js'
@@ -46,16 +47,14 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [
-        {
-            maxInstances: 1,
-            browserName: 'chrome',
-        },
-        {
-            maxInstances: 1,
-            browserName: 'firefox',
-        }
-    ],
+    capabilities: [{
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 5,
+        //
+        browserName: 'firefox'
+    }],
     //
     // ===================
     // Test Configurations
